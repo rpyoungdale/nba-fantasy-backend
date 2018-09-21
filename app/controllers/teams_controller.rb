f5c1184d870bbdaa312e4d02ef921e5ffb93f92b
rpyoungdale <youngdale95@gmail.com>
2018-09-21 15:54:44 -0500

    gets game data and sends to frontend

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,2 +1,10 @@
 class TeamsController < ApplicationController
+  skip_before_action :authorized, only: [:game_scores]
+
+  def game_scores
+    #{params[:current_date]}
+    games = RestClient.get("http://data.nba.net/10s/prod/v2/20181017/scoreboard.json")
+    # byebug
+    render json: games
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get '/user', to: 'users#show_user'
   get '/all-teams', to: 'users#all_teams'
   get '/find-player', to: 'players#find_player'
+  post '/game-scores', to: 'teams#game_scores'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
